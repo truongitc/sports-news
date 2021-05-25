f22c053f4ae0ccac40b76dee7661d31e98b1fdcf
truongnv <nguyentruong10793@gmail.com>
2021-05-25 18:46:34 +0700

    add project

---
+++ global-news/src/main/java/co/global/gin/SportsNewsApplication.java
@@ -0,0 +1,13 @@
+package co.global.gin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsNewsApplication.class, args);
+	}
+
+}
---
+++ global-news/src/test/java/co/global/gin/SportsNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.global.gin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
